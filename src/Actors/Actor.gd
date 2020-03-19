3cf7f6074d842ca88227aebc9a7f80b4d6e565b4
GGOOTEE <59986488+GGOOTEE@users.noreply.github.com>
2020-03-18 21:29:29 -0400

    current

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -1,5 +1,3 @@
-extends Node
-
 extends KinematicBody2D
 class_name Actor
 
@@ -7,7 +5,7 @@ class_name Actor
 const FLOOR_NORMAL: = Vector2.UP
 
 export var speed: = Vector2(400.0, 500.0)
-export var gravity: = 3500.0
+export var gravity = 3500.0
 
 var _velocity: = Vector2.ZERO
 
